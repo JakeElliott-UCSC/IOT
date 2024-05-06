38170f592bd33bed30997a47d4c5499a2cdcb5bd
Jake <jake.33.Elliott@gmail.com>
2024-05-06 12:58:01 -0700

    moved constants to the robot cpp file

--- Lab3/lab3_2/main/DFRobot_LCD.h
+++ Lab3/lab3_2/main/DFRobot_LCD.h
@@ -95,10 +95,22 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS 0x00
 
+// Constants
+#define I2C_MASTER_SCL_IO    8    /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO    10    /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM       I2C_NUM_0 /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ   100000   /*!< I2C master clock frequency */
+#define SHTC3_SENSOR_ADDR    0x70     /*!< Slave address of the SHTC3 sensor */
+
+#define ACK_CHECK_EN         0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS        0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL              0x0     /*!< I2C ack value */
+#define NACK_VAL             0x1     /*!< I2C nack value */
+
 
 //class DFRobot_LCD : public Print {
 class DFRobot_LCD {
-  
+
 public:
 
   /*!
