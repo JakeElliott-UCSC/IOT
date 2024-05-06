d974016e7491a8e45617e5e31a637615fedd8e73
Jake <jake.33.Elliott@gmail.com>
2024-05-06 13:07:35 -0700

    initialized i2c struct to zeros

--- Lab3/lab3_2/main/lab3_2.cpp
+++ Lab3/lab3_2/main/lab3_2.cpp
@@ -17,7 +17,12 @@ static esp_err_t i2c_master_init(void)
 {
     i2c_port_t i2c_master_port = I2C_MASTER_NUM;
     i2c_config_t conf = {};
-    
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
     //  {
     //     .mode = I2C_MODE_MASTER,
     //     .sda_io_num = I2C_MASTER_SDA_IO,
