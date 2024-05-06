0eecfcd7d6d8b1490f5bff17a830cc57fcef5f1c
Jake <Jelliot1@ucsc.edu>
2024-05-06 03:47:30 +0000

    created project for lab 3

---
+++ Lab3/lab3_2/main/lab3_2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+
+// Constants
+#define I2C_MASTER_SCL_IO    8    /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO    10    /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM       I2C_NUM_0 /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ   100000   /*!< I2C master clock frequency */
+#define SHTC3_SENSOR_ADDR    0x70     /*!< Slave address of the SHTC3 sensor */
+
+#define ACK_CHECK_EN         0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS        0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL              0x0     /*!< I2C ack value */
+#define NACK_VAL             0x1     /*!< I2C nack value */
+
+static const char *TAG = "main";
+
+/**
+ * @brief i2c master initialization
+ */
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+}
+
+
+
+
+void app_main(void)
+{
+    // initialize i2c on the chip
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C Initialized Successfully");
+
+    while (1) {
+        printf("Hello World - Lab 3.3\n");
+        vTaskDelay(pdMS_TO_TICKS(2000)); // Poll every 2 seconds
+    }
+}
