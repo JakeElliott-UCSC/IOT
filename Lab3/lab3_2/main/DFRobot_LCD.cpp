5a0916a6e46cb1d2deff5bfce90f6a3a3bf11d97
Jake <jake.33.Elliott@gmail.com>
2024-05-06 17:27:36 -0700

    trying to get the LCD to not return error

--- Lab3/lab3_2/main/DFRobot_LCD.cpp
+++ Lab3/lab3_2/main/DFRobot_LCD.cpp
@@ -330,13 +330,18 @@ void DFRobot_LCD::setReg(uint8_t addr, uint8_t data)
     i2c_master_start(cmd); // transmit to device #4
     //i2c_master_write_byte(cmd, (addr << 1), ACK_CHECK_EN);
     i2c_master_write_byte(cmd, (_RGBAddr<<1), ACK_CHECK_EN);
+    vTaskDelay(pdMS_TO_TICKS(5));
     // RGB address for V2.0 LCD screen
     //i2c_master_write_byte(cmd, 0x2D, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, 0x80, ACK_CHECK_EN);
     vTaskDelay(pdMS_TO_TICKS(5));
     i2c_master_write_byte(cmd, addr, ACK_CHECK_EN);
-    
+    vTaskDelay(pdMS_TO_TICKS(5));
+
+    i2c_master_write_byte(cmd, 0x80, ACK_CHECK_EN);
     vTaskDelay(pdMS_TO_TICKS(5));
     i2c_master_write_byte(cmd, data, ACK_CHECK_EN);
+
     i2c_master_stop(cmd);    // stop transmitting
 
     ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
