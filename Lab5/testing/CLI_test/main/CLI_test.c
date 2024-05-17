c0a2656c9468a32bec993a0f2aa01648921a83a1
Jake <jake.33.Elliott@gmail.com>
2024-05-17 07:53:28 -0700

    changed main file to only print the first byte of data

--- Lab5/testing/CLI_test/main/CLI_test.c
+++ Lab5/testing/CLI_test/main/CLI_test.c
@@ -51,7 +51,7 @@ void app_main(void) {
         if (len > 0) {
             data[len] = '\0'; // Null-terminate the received data
             //parse_and_execute_command((char *)data);
-            printf("first character: %c",data[0]);
+            printf("first character: %c\n",data[0]);
         }
         vTaskDelay(100 / portTICK_PERIOD_MS); // Delay to prevent task from consuming too much CPU
     }
