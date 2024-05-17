20422363edbea9f0e59c1c3c1e67d2ac23e35102
Jake <jake.33.Elliott@gmail.com>
2024-05-16 21:53:04 -0700

    added practice adc code

--- Lab5/lab5_1/main/lab5_1.c
+++ Lab5/lab5_1/main/lab5_1.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define ADC_CHANNEL ADC1_CHANNEL_0  // GPIO 0 (ADC1_CH0) is used for reading
+#define LED_PIN GPIO_NUM_2          // GPIO 2 is used for the LED
+#define ADC_THRESHOLD 2000          // Threshold for turning on the LED
+
+static const char *TAG = "ADC_LED_Example";
 
 void app_main(void)
 {
+    // Configure ADC
+    adc1_config_width(ADC_WIDTH_BIT_12);  // Set ADC width to 12 bits
+    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_0);  // Set attenuation to 0dB
 
+    // Configure LED pin
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        // liveliness signal
+        ESP_LOGI(TAG, "Hello World");
+        // set gpio pin 2 to high
+        gpio_set_level(LED_PIN, 1);
+        // Delay for 1 second
+        vTaskDelay(pdMS_TO_TICKS(1000));  
+
+
+        // int adc_value = adc1_get_raw(ADC_CHANNEL);  // Get ADC value
+        // ESP_LOGI(TAG, "ADC Value: %d", adc_value);  // Log ADC value
+
+        // if (adc_value > ADC_THRESHOLD) {
+        //     gpio_set_level(LED_PIN, 1);  // Turn on LED
+        // } else {
+        //     gpio_set_level(LED_PIN, 0);  // Turn off LED
+        // }
+
+        // vTaskDelay(pdMS_TO_TICKS(1000));  // Delay for 1 second
+    }
 }
