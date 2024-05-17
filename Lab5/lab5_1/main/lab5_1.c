828f610b59b77dc68da4d3ba6d4cd2dd9010a618
Jake <jake.33.Elliott@gmail.com>
2024-05-17 03:37:29 -0700

    testing print character

--- Lab5/lab5_1/main/lab5_1.c
+++ Lab5/lab5_1/main/lab5_1.c
@@ -4,6 +4,7 @@
 #include "driver/adc.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include <math.h>
 
 #define ADC_CHANNEL ADC1_CHANNEL_0  // GPIO 0 (ADC1_CH0) is used for reading
 #define LED_PIN GPIO_NUM_2          // GPIO 2 is used for the LED
@@ -50,7 +51,7 @@ void printMorseString(const char* message){
 
     while (message[x] != NULL) {
 
-        x++
+        x++;
         if (x > 15) {
             ESP_LOGE(TAG, "ERROR: printMorseString exceeded 15 iterations");
             break;
