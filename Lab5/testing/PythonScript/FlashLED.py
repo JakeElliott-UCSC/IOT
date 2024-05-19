c61369ff243f78c92b9653074dab8a28caaf4266
Jake <jake.33.Elliott@gmail.com>
2024-05-19 16:56:49 -0700

    making the LED flash quicker

--- Lab5/testing/PythonScript/FlashLED.py
+++ Lab5/testing/PythonScript/FlashLED.py
@@ -4,11 +4,11 @@ import time
 led = gpiozero.LED(17)
 
 x = 0
-#time_delta = 0.25
+time_delta = 0.2
 
 while x < 20:
     led.on()
-    time.sleep(0.25)
+    time.sleep(time_delta)
     led.off()
-    time.sleep(0.25)
+    time.sleep(time_delta)
     x = x+1
