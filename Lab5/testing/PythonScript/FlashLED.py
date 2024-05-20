caa69cc9c3527fac786c903a329ba03c639992d6
Jake <jake.33.Elliott@gmail.com>
2024-05-19 18:21:53 -0700

    adding CLI arguents

--- Lab5/testing/PythonScript/FlashLED.py
+++ Lab5/testing/PythonScript/FlashLED.py
@@ -115,8 +115,8 @@ print("from Python: " + sys.argv[2])
 
 
 
-for rep in range(sys.argv[1]):
-    printMorse(int(sys.argv[2]))
+for rep in range(int(sys.argv[1])):
+    printMorse(sys.argv[2])
 
 
 printMorse("Hello World")
