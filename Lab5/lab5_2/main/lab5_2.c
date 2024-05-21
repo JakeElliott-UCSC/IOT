92134325a5da5b7b50d90e761fe7f9313f7ef922
Jake <jake.33.Elliott@gmail.com>
2024-05-20 20:59:02 -0700

    trying to read morse

--- Lab5/lab5_2/main/lab5_2.c
+++ Lab5/lab5_2/main/lab5_2.c
@@ -153,7 +153,7 @@ void app_main(void)
         //printf("charIncoming: %d\n",charIncoming);
         if (charIncoming) {
             printMorse(morseSignal);
-            printf("\n");
+            //printf("\n");
             if (morseSignal == 1 || morseSignal == 0) {
                 charIncoming = 0;
             }
