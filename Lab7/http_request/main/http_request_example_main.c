f2117f05da14ab4a069c0d94c264b4889651f001
Jake <jake.33.Elliott@gmail.com>
2024-06-04 06:00:08 -0700

    testing get and put in esp

--- Lab7/http_request/main/http_request_example_main.c
+++ Lab7/http_request/main/http_request_example_main.c
@@ -18,12 +18,18 @@
 /* Constants that aren't configurable in menuconfig */
 #define WEB_SERVER "100.80.129.212"
 #define WEB_PORT "5000"
-#define POST_PATH "/weather"
 #define GET_PATH "/"
+#define POST_PATH "/weather"
+#define WEATHER_GET_PATH "/weather"
 
 static const char *TAG = "example";
 
 static const char *REQUEST_GET = "GET " GET_PATH " HTTP/1.0\r\n"
+    "Host: " WEB_SERVER "\r\n"
+    "User-Agent: esp-idf/1.0 esp32\r\n"
+    "\r\n";
+
+static const char *REQUEST_WEATHER_GET = "GET " WEATHER_GET_PATH " HTTP/1.0\r\n"
     "Host: " WEB_SERVER "\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
     "\r\n";
@@ -153,6 +159,66 @@ void http_post_request(int temp)
         bzero(recv_buf, sizeof(recv_buf));
         r = read(s, recv_buf, sizeof(recv_buf) - 1);
         if (r > 0) {
+            for (int i = 0; r > 0; i++) {
+                putchar(recv_buf[i]);
+            }
+        }
+    } while (r > 0);
+
+    close(s);
+}
+
+void get_weather(void)
+{
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+    if (err != 0 || res == NULL) {
+        return;
+    }
+
+    addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+
+    s = socket(res->ai_family, res->ai_socktype, 0);
+    if (s < 0) {
+        freeaddrinfo(res);
+        return;
+    }
+
+    if (connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+        close(s);
+        freeaddrinfo(res);
+        return;
+    }
+
+    freeaddrinfo(res);
+
+    if (write(s, REQUEST_WEATHER_GET, strlen(REQUEST_WEATHER_GET)) < 0) {
+        close(s);
+        return;
+    }
+
+    struct timeval receiving_timeout;
+    receiving_timeout.tv_sec = 5;
+    receiving_timeout.tv_usec = 0;
+    if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                   sizeof(receiving_timeout)) < 0) {
+        close(s);
+        return;
+    }
+
+    /* Read HTTP response */
+    do {
+        bzero(recv_buf, sizeof(recv_buf));
+        r = read(s, recv_buf, sizeof(recv_buf) - 1);
+        if (r > 0) {
             for (int i = 0; i < r; i++) {
                 putchar(recv_buf[i]);
             }
@@ -172,4 +238,5 @@ void app_main(void)
 
     http_get_request();
     http_post_request(25); // Example temperature value
+    get_weather();
 }
