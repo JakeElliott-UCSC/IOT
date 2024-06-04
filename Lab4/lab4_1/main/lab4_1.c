d1268869947cf0ef2b4100a2b8c62874acc9c861
Jake <jake.33.Elliott@gmail.com>
2024-06-04 14:47:12 -0700

    got chat gpt to use the provided functions

--- Lab4/lab4_1/main/lab4_1.c
+++ Lab4/lab4_1/main/lab4_1.c
@@ -11,6 +11,7 @@
 #define I2C_MASTER_NUM       I2C_NUM_0 /*!< I2C port number for master dev */
 #define I2C_MASTER_FREQ_HZ   100000   /*!< I2C master clock frequency */
 #define SHTC3_SENSOR_ADDR    0x70     /*!< Slave address of the SHTC3 sensor */
+#define IMU_SENSOR_ADDR      0x68     /*!< Slave address of the IMU sensor */
 
 #define ACK_CHECK_EN         0x1     /*!< I2C master will check ack from slave*/
 #define ACK_CHECK_DIS        0x0     /*!< I2C master will not check ack from slave */
@@ -39,35 +40,61 @@ static esp_err_t i2c_master_init(void)
 
 void app_main(void)
 {
-    float accel_x, accel_y, accel_z;
-    ESP_ERROR_CHECK(i2c_master_init());
+    // Initialize I2C
+    i2c_master_init();
 
+    // Create ICM42670 sensor instance
+    icm42670_handle_t sensor = icm42670_create(I2C_MASTER_NUM, ICM42670_ID);
+    if (sensor == NULL) {
+        ESP_LOGI(TAG, "Failed to initialize ICM42670");
+        return;
+    }
 
-    // Initialize the ICM42670
-    // if (icm42670_init() != ESP_OK) {
-    //     ESP_LOGI(TAG, "Failed to initialize ICM42670");
-    //     return;
-    // }
+    // Configure the ICM42670 sensor
+    icm42670_cfg_t icm_config = {
+        .gyro_fs = GYRO_FS_250DPS,
+        .gyro_odr = 0x0B,
+        .acce_fs = ACCE_FS_2G,
+        .acce_odr = 0x0A
+    };
+    if (icm42670_config(sensor, &icm_config) != ESP_OK) {
+        ESP_LOGI(TAG, "Failed to configure ICM42670");
+        return;
+    }
 
-    // while (1) {
-    //     // Read accelerometer data
-    //     if (icm42670_read_accel(&accel_x, &accel_y, &accel_z) == ESP_OK) {
-    //         // Determine the inclination
-    //         if (accel_x > 0.5) {
-    //             ESP_LOGI(TAG, "UP");
-    //         } else if (accel_x < -0.5) {
-    //             ESP_LOGI(TAG, "DOWN");
-    //         }
+    // Set accelerometer power mode
+    if (icm42670_acce_set_pwr(sensor, ICM42670_ACCE_PWR_ON) != ESP_OK) {
+        ESP_LOGI(TAG, "Failed to set accelerometer power mode");
+        return;
+    }
 
-    //         if (accel_y > 0.5) {
-    //             ESP_LOGI(TAG, "RIGHT");
-    //         } else if (accel_y < -0.5) {
-    //             ESP_LOGI(TAG, "LEFT");
-    //         }
-    //     } else {
-    //         ESP_LOGI(TAG, "Failed to read accelerometer data");
-    //     }
+    // Set gyroscope power mode
+    if (icm42670_gyro_set_pwr(sensor, ICM42670_GYRO_PWR_ON) != ESP_OK) {
+        ESP_LOGI(TAG, "Failed to set gyroscope power mode");
+        return;
+    }
 
-    //     vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
-    // }
+    icm42670_value_t acce_value;
+
+    while (1) {
+        // Read accelerometer data
+        if (icm42670_get_acce_value(sensor, &acce_value) == ESP_OK) {
+            // Determine the inclination
+            if (acce_value.x > 0.5) {
+                ESP_LOGI(TAG, "UP");
+            } else if (acce_value.x < -0.5) {
+                ESP_LOGI(TAG, "DOWN");
+            }
+
+            if (acce_value.y > 0.5) {
+                ESP_LOGI(TAG, "RIGHT");
+            } else if (acce_value.y < -0.5) {
+                ESP_LOGI(TAG, "LEFT");
+            }
+        } else {
+            ESP_LOGI(TAG, "Failed to read accelerometer data");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+    }
 }
