6b606e47a377c2f0eaad2da9d9d4e15a329b7c9c
Jake <jake.33.Elliott@gmail.com>
2024-06-04 15:58:22 -0700

    changed some constants

--- Lab4/lab4_1/main/lab4_1.c
+++ Lab4/lab4_1/main/lab4_1.c
@@ -12,7 +12,7 @@
 #define I2C_MASTER_NUM       I2C_NUM_0 /*!< I2C port number for master dev */
 #define I2C_MASTER_FREQ_HZ   100000   /*!< I2C master clock frequency */
 #define SHTC3_SENSOR_ADDR    0x70     /*!< Slave address of the SHTC3 sensor */
-#define IMU_SENSOR_ADDR      0x68     /*!< Slave address of the IMU sensor */
+#define IMU_SENSOR_ADDR      0x67     /*!< Slave address of the IMU sensor */
 
 #define ACK_CHECK_EN         0x1     /*!< I2C master will check ack from slave*/
 #define ACK_CHECK_DIS        0x0     /*!< I2C master will not check ack from slave */
