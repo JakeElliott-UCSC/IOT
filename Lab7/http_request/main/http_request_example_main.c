424fcc5f94ed3bf2aceace5761adb5d188e2862a
Jake <jake.33.Elliott@gmail.com>
2024-06-04 05:13:37 -0700

    asked chatgpt to simplify the output

--- Lab7/http_request/main/http_request_example_main.c
+++ Lab7/http_request/main/http_request_example_main.c
@@ -20,14 +20,12 @@
 #define WEB_PORT "5000"
 #define WEB_PATH "/"
 
-static const char *TAG = "example";
-
 static const char *REQUEST = "GET " WEB_PATH " HTTP/1.0\r\n"
     "Host: " WEB_SERVER "\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
     "\r\n";
 
-static void http_get_task(void *pvParameters)
+void http_get_request(void)
 {
     const struct addrinfo hints = {
         .ai_family = AF_INET,
@@ -38,71 +36,62 @@ static void http_get_task(void *pvParameters)
     int s, r;
     char recv_buf[64];
 
-    while(1) {
-        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
-
-        if(err != 0 || res == NULL) {
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            freeaddrinfo(res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        freeaddrinfo(res);
-
-        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        struct timeval receiving_timeout;
-        receiving_timeout.tv_sec = 5;
-        receiving_timeout.tv_usec = 0;
-        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                sizeof(receiving_timeout)) < 0) {
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            if (r > 0) {
-                for(int i = 0; i < r; i++) {
-                    putchar(recv_buf[i]);
-                }
-            }
-        } while(r > 0);
-
-        close(s);
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
+    int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+    if (err != 0 || res == NULL) {
+        return;
     }
+
+    addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+
+    s = socket(res->ai_family, res->ai_socktype, 0);
+    if (s < 0) {
+        freeaddrinfo(res);
+        return;
+    }
+
+    if (connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+        close(s);
+        freeaddrinfo(res);
+        return;
+    }
+
+    freeaddrinfo(res);
+
+    if (write(s, REQUEST, strlen(REQUEST)) < 0) {
+        close(s);
+        return;
+    }
+
+    struct timeval receiving_timeout;
+    receiving_timeout.tv_sec = 5;
+    receiving_timeout.tv_usec = 0;
+    if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                   sizeof(receiving_timeout)) < 0) {
+        close(s);
+        return;
+    }
+
+    /* Read HTTP response */
+    do {
+        bzero(recv_buf, sizeof(recv_buf));
+        r = read(s, recv_buf, sizeof(recv_buf) - 1);
+        if (r > 0) {
+            for (int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        }
+    } while (r > 0);
+
+    close(s);
 }
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
+    ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     ESP_ERROR_CHECK(example_connect());
 
-    xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+    http_get_request();
 }
