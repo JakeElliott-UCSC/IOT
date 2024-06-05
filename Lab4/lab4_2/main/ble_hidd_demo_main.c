ca10a73c9858d6f624c05797ca13f9d21ff5aefa
Jake <jake.33.Elliott@gmail.com>
2024-06-04 19:29:14 -0700

    added mouse code

--- Lab4/lab4_2/main/ble_hidd_demo_main.c
+++ Lab4/lab4_2/main/ble_hidd_demo_main.c
@@ -51,7 +51,7 @@
 
 static uint16_t hid_conn_id = 0;
 static bool sec_conn = false;
-static bool send_volum_up = false;
+static bool move_mouse_left = false;
 #define CHAR_DECLARATION_SIZE   (sizeof(uint8_t))
 
 static void hidd_event_callback(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param);
@@ -172,17 +172,22 @@ void hid_demo_task(void *pvParameters)
         vTaskDelay(2000 / portTICK_PERIOD_MS);
         if (sec_conn) {
             ESP_LOGI(HID_DEMO_TAG, "Send the volume");
-            send_volum_up = true;
+            move_mouse_left = true;
             //uint8_t key_vaule = {HID_KEY_A};
             //esp_hidd_send_keyboard_value(hid_conn_id, 0, &key_vaule, 1);
-            esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_UP, true);
+            //esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_UP, true);
+            // esp_hidd_send_mouse_value(hid_conn_id,0);
+            esp_hidd_send_mouse_value(hid_conn_id, 0, -10, 0, 0, 0, 0);  // Move mouse left
             vTaskDelay(3000 / portTICK_PERIOD_MS);
-            if (send_volum_up) {
-                send_volum_up = false;
-                esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_UP, false);
-                esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_DOWN, true);
+            if (move_mouse_left) {
+                move_mouse_left = false;
+                esp_hidd_send_mouse_value(hid_conn_id, 0, 0, 0, 0, 0, 0);  // Stop mouse
+                esp_hidd_send_mouse_value(hid_conn_id, 0, 10, 0, 0, 0, 0);  // Move mouse right
+                //esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_UP, false);
+                //esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_DOWN, true);
                 vTaskDelay(3000 / portTICK_PERIOD_MS);
-                esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_DOWN, false);
+                //esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_DOWN, false);
+                esp_hidd_send_mouse_value(hid_conn_id, 0, 0, 0, 0, 0, 0);  // Stop mouse
             }
         }
     }
