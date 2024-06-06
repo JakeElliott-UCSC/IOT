28595db8bf37e6d182c81a143306924c31fb5010
Jake <jake.33.Elliott@gmail.com>
2024-06-05 22:32:03 -0700

    added blink LED code

--- Lab8/lab8_3/src/main.rs
+++ Lab8/lab8_3/src/main.rs
@@ -1,10 +1,21 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
 
-    log::info!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::output(peripherals.pins.gpio7)?;
+
+    println!("Hello world!");
+    loop {
+        led.set_high()?;
+        // we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(1000);
+
+        led.set_low()?;
+        FreeRtos::delay_ms(1000);
+    }
 }
