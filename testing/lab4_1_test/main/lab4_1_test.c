7661219d82d8c253d739be241fcd08beccfe07bb
Jake <jake.33.Elliott@gmail.com>
2024-06-07 14:43:50 -0700

    testing read

--- testing/lab4_1_test/main/lab4_1_test.c
+++ testing/lab4_1_test/main/lab4_1_test.c
@@ -280,7 +280,7 @@ void app_main(void)
     printf("entering while loop\n");
     while (1) {
 
-        read_gyro(gyroX,gyroY,gyroZ);
+        read_gyro(&gyroX,&gyroY,&gyroZ);
         printf("Gyro x, y, z: %d, %d, %d\n",gyroX,gyroY,gyroZ);
         // if (icm42670_get_gyro_value(sensor, &gyro) != ESP_OK ) {
         //     ESP_LOGE(TAG, "get gyro failed");
